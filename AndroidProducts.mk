7e0cb7d3589f2dde1fb5b5e17b279fc7db62af34
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 06:56:58 +0100

    TECNO-CG8: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: tecno Model: TECNO CG8 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-CG8.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-CG8-user \
+    omni_TECNO-CG8-userdebug \
+    omni_TECNO-CG8-eng
